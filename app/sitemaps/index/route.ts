2879c816c7badbec5cfc9013a8a4f769e7217325
abmccull <handscrapedflooring@gmail.com>
2025-08-14 12:19:42 -0600

    fix: correct XML formatting in sitemap index

--- app/sitemaps/index/route.ts
+++ app/sitemaps/index/route.ts
@@ -13,31 +13,29 @@ export async function GET() {
   const base = process.env.NEXT_PUBLIC_SITE_URL || "https://wildwestslc.com";
   const now = new Date().toISOString();
   
-  const body = xml`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <sitemap>
-        <loc>${base}/sitemaps/core</loc>
-        <lastmod>${now}</lastmod>
-      </sitemap>
-      <sitemap>
-        <loc>${base}/sitemaps/locations</loc>
-        <lastmod>${now}</lastmod>
-      </sitemap>
-      <sitemap>
-        <loc>${base}/sitemaps/services</loc>
-        <lastmod>${now}</lastmod>
-      </sitemap>
-      <sitemap>
-        <loc>${base}/sitemaps/blog</loc>
-        <lastmod>${now}</lastmod>
-      </sitemap>
-      <sitemap>
-        <loc>${base}/sitemaps/pages</loc>
-        <lastmod>${now}</lastmod>
-      </sitemap>
-    </sitemapindex>
-  `;
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${base}/sitemaps/core</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${base}/sitemaps/locations</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${base}/sitemaps/services</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${base}/sitemaps/blog</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${base}/sitemaps/pages</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+</sitemapindex>`;
   return new NextResponse(body, {
     headers: { "Content-Type": "application/xml; charset=utf-8" },
   });
